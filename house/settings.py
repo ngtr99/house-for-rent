eb6d2519ca062b93b65463df48d03fc96e75dfd8
Nguyệt Trang <nguyet@Nguyets-Laptop.local>
2025-03-29 20:48:59 -0700

    add railway domain to allowed_host

--- house/settings.py
+++ house/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['house-for-rent-production.up.railway.app/']
+ALLOWED_HOSTS = ['house-for-rent-production.up.railway.app']
 
 
 # Application definition
